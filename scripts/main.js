8cf98bf861d27c399da1554cb2f79f2ebd1f67a9
ldabiralai <lawrence.dabir@gmail.com>
2013-08-06 22:14:03 +0100

    working on json

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,24 @@ $(document).ready(function () { Main.run(); });
 
 var Main = {
 	run: function () {
-		
+		console.log("running...");
+		JenkinsJSON.run();
+	}
+}
+
+var JenkinsJSON = {
+
+	mainJenkinsUrl: "http://10.0.0.80:8080",
+
+	run: function () {
+		console.log("JSON functions running");
+		console.log(JenkinsJSON.mainJenkinsUrl);
+		JenkinsJSON.getData();
+	},
+
+	getData: function () {
+		$.getJSON(JenkinsJSON.mainJenkinsUrl + "/api/json", function(json) {
+			console.log("Result: " + json.jobs.color);
+		})
 	}
 }
